b1c6a52865f8d1bc79e8f56b5c241dc4a7a3b236
gleafy <greenleafy@yandex.ru>
2025-09-19 17:23:22 +0400

    Обновил админки

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal", {"fields": ("username", "first_name", "last_name", "avatar", "phone", "country")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = ((None, {"fields": ("email", "username", "password1", "password2")}),)
+    list_display = ("email", "username", "is_staff")
+    search_fields = ("email", "username")
+    ordering = ("email",)
